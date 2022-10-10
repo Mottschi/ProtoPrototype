543eba1e3178c865fa73fcff96f0d284d465d9ba
Sebastian Mottschall <srophkor@gmail.com>
2022-10-10 14:41:47 +0200

    Trying out Pip to replace Poetry on render

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
-poetry install
+pip install -r requirements.txt
+
+#poetry install
 
 #necessary to fix issue caused by outdated poetry version on render.com:
-pip install setuptools
+#pip install setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
